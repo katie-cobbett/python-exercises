ab20ecd2f3d4c53fb7cc890af23feb6ed2326b4a
katie-cobbett <katie.cobbett@thecobbett.net>
2022-10-16 22:06:35 +0100

    zigzag string problem

--- 17-zigzag_string/solution.py
+++ 17-zigzag_string/solution.py
@@ -4,14 +4,65 @@ class Solution:
         for number in number:
             list
 
-    def convert(self, s: str, numRows: int) -> str:
-        string = s 
-        number_rows = numRows 
-        list_of_characters = ()
-        new_string = ""
-        for character in string:
-            #making a list with the characters spaced out and seperate items 
-            list_of_characters.append(character)
-        for row in number_rows:
+    def convert(self, input_string: str, rows: int) -> str:
+# amount of intermediate (singly-filled) lists in between two possibly full comumns = number_rows - 2
 
-        return new_string 
+        # characters = []
+        
+        # for index in range(0,len(input_string)): 
+
+        columns = [] # [ ['c','a','t'],['d'] ]
+        current_column = [] # 
+        is_fill_full_column = True
+        num_singly_filled_columns = 0
+
+        for character in input_string:
+            #making a list with the characters spaced out and seperate items 
+            # characters.append(character)
+            
+            if is_fill_full_column:
+                current_column.append(character)
+                if len(current_column) >= rows:
+                    columns.append(current_column)
+                    current_column = []
+                    if rows >= 3:
+                        is_fill_full_column = False
+                        num_singly_filled_columns = rows-2
+                    else:
+                        is_fill_full_column = True
+                    
+            else: 
+                for _ in range(0,num_singly_filled_columns): # for something in a loop that you don't care about, can put _ 
+                    current_column.append(' ')
+                current_column.append(character)
+                columns.append(current_column)
+                current_column = []
+                num_singly_filled_columns -=1
+                if num_singly_filled_columns <= 0 :
+                    is_fill_full_column = True
+
+            #print("Current:{}".format(current_column))
+            #print("columns:{}".format(columns))
+            #print("is_fill_full_column:{}".format(is_fill_full_column))
+
+        columns.append(current_column)
+        print("Final columns:{}".format(columns))
+
+
+        # We have a list of column lists...
+        # Now we need to extract the data back into a results string...
+
+
+        # for row in number_rows: ########
+        # maybe need to 
+        results_string = ""
+
+        # [c, a, t]
+        # [ , d]
+        for i in range(0,rows):
+            for column in columns:
+                if len(column) > i:
+                    if column[i] != ' ':
+                        results_string += column[i]
+        
+        return results_string 
--- 17-zigzag_string/test_solution.py
+++ 17-zigzag_string/test_solution.py
@@ -3,6 +3,86 @@ from solution import Solution
 solution= Solution()
 
 def test_that_anything_is_made():
-    s= "cat"
-    numRows = 3
-    assert_that(solution.convert(s,numRows)).is_equal_to("cat")
+    input_string = "cat"
+    #  c
+    #  a    
+    #  t
+    rows = 3
+    assert_that(solution.convert(input_string,rows)).is_equal_to("cat")
+
+def test_catdog():
+    input_string = "catdog"
+     # c   o
+     # a d g
+     # t
+    rows = 3
+    assert_that(solution.convert(input_string,rows)).is_equal_to("coadgt")
+
+def test_catd():
+    input_string = "catd"
+    # c   
+    # a d 
+    # t
+    rows = 3
+    assert_that(solution.convert(input_string,rows)).is_equal_to("cadt")
+    
+def test_given_example_1():
+    input_string = "PAYPALISHIRING"
+    # P   A   H   N
+    # A P L S I I G
+    # Y   I   R
+    rows = 3
+    assert_that(solution.convert(input_string,rows)).is_equal_to("PAHNAPLSIIGYIR")
+
+def test_larger_rows(): 
+    input_string = "longercat"
+    rows = 4
+    # l     c
+    # o   r a
+    # n e   t
+    # g
+    assert_that(solution.convert(input_string, rows)).is_equal_to("lcoranetg")
+
+def test_katie_hard_test_rows_6():
+    input_string = "thistestisreallylong6"
+    # t        r        6
+    # h      s e      g
+    # i     i  a     n
+    # s   t    l   o
+    # t s      l l
+    # e        y
+    rows = 6
+    assert_that(solution.convert(input_string,rows)).is_equal_to("tr6hsegiianstlotslley")
+    
+    
+def test_given_problem_2():
+    input_string = "PAYPALISHIRING"
+    rows = 4
+    # P     I    N
+    # A   L S  I G
+    # Y A   H R
+    # P     I
+    assert_that(solution.convert(input_string,rows)).is_equal_to("PINALSIGYAHRPI")
+
+
+def test_given_problem_3():
+    input_string = "A"
+    rows = 1
+    # A
+    assert_that(solution.convert(input_string,rows)).is_equal_to("A")
+    
+
+
+def test_long_thing_one_row():
+    input_string = "ABCD"
+    rows = 1
+    # A B C D
+    assert_that(solution.convert(input_string,rows)).is_equal_to("ABCD")
+
+
+def test_long_thing_two_rows():
+    input_string = "ABCD"
+    rows = 2
+    # A C
+    # B D
+    assert_that(solution.convert(input_string,rows)).is_equal_to("ACBD")
